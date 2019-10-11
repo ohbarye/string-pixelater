3d6882abe24ff933072fcb6387a2c5fa528e7027
ohbarye <over.rye@gmail.com>
2019-10-11 23:45:10 +0900

    yarn build

--- dist/js/string-pixelater.js
+++ dist/js/string-pixelater.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    (global.StringPixelater = factory());
-}(this, (function () { 'use strict';
+    (global = global || self, global.StringPixelater = factory());
+}(this, function () { 'use strict';
 
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation. All rights reserved.
@@ -19,12 +19,15 @@
     and limitations under the License.
     ***************************************************************************** */
 
-    var __assign = Object.assign || function __assign(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
-        }
-        return t;
+    var __assign = function() {
+        __assign = Object.assign || function __assign(t) {
+            for (var s, i = 1, n = arguments.length; i < n; i++) {
+                s = arguments[i];
+                for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+            }
+            return t;
+        };
+        return __assign.apply(this, arguments);
     };
 
     var DEFALUT_OPTIONS = {
@@ -36,7 +39,7 @@
         function StringPixelater(str, options) {
             if (options === void 0) { options = {}; }
             this.str = str;
-            this.options = __assign({}, DEFALUT_OPTIONS, options);
+            this.options = __assign(__assign({}, DEFALUT_OPTIONS), options);
             this.canvas = document.createElement('canvas');
             this.context = this.canvas.getContext('2d');
             if (!str) {
@@ -108,4 +111,4 @@
 
     return StringPixelater;
 
-})));
+}));
